ef5784c7e4e4fd232570b243b7066676ee3b1996
Enkhmanlai Ganzorig <manlaig@users.noreply.github.com>
2017-11-04 22:14:51 -0700

    Displaying the menu on the screen

--- app/src/main/java/com/example/android/chatly/MainActivity.java
+++ app/src/main/java/com/example/android/chatly/MainActivity.java
@@ -1,13 +1,63 @@
 package com.example.android.chatly;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ListView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String ANONYMOUS = "anonymous";
+    public static final int RC_SIGN_IN = 1;
+
+    private ListView messageListView;
+    private ImageButton imageButton;
+    private EditText editTextField;
+    private Button sendButton;
+
+    private String username;
+
+    private FirebaseAuth firebaseAuth;
+    private FirebaseDatabase firebaseDatabase;
+    private DatabaseReference databaseReference;
+    private FirebaseAuth.AuthStateListener authStateListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initializeAllInstanceVariables();
     }
+
+    private void initializeAllInstanceVariables()
+    {
+        messageListView = findViewById(R.id.messageListView);
+        imageButton = findViewById(R.id.photoPickerButton);
+        editTextField = findViewById(R.id.messageEditText);
+        sendButton = findViewById(R.id.sendButton);
+
+        username = ANONYMOUS;
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference().child("messages");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
 }
