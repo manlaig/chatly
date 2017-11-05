1f28229396404ee3c917ec29b08a69293ec9548f
Enkhmanlai Ganzorig <manlaig@users.noreply.github.com>
2017-11-04 23:59:04 -0700

    Creating a custom class for each message

---
+++ app/src/main/java/com/example/android/chatly/Message.java
@@ -0,0 +1,43 @@
+package com.example.android.chatly;
+
+/**
+ * Created by manlai on 11/4/2017.
+ */
+
+public class Message
+{
+    private String text;
+    private String name;
+    private String photoURL;
+
+    public Message(String text, String name, String photoURL)
+    {
+        this.text = text;
+        this.name = name;
+        this.photoURL = photoURL;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhotoURL() {
+        return photoURL;
+    }
+
+    public void setPhotoURL(String photoURL) {
+        this.photoURL = photoURL;
+    }
+}
